a18c2a78120b0d069ad42fb446c2c2e7a7e46aca
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2017-02-20 14:12:10 +1030

    Added validation Added support for modifying the object metadata Added support for adding and modifying data streams

--- src/main/java/com/googlecode/fascinator/redbox/ws/v1/resources/HarvestResource.java
+++ src/main/java/com/googlecode/fascinator/redbox/ws/v1/resources/HarvestResource.java
@@ -3,17 +3,18 @@ package com.googlecode.fascinator.redbox.ws.v1.resources;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.fusesource.hawtbuf.ByteArrayInputStream;
 import org.json.simple.JSONArray;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.resource.Post;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.googlecode.fascinator.api.PluginException;
 import com.googlecode.fascinator.api.indexer.Indexer;
@@ -29,10 +30,9 @@ import com.googlecode.fascinator.common.JsonSimpleConfig;
 import com.googlecode.fascinator.common.messaging.MessagingException;
 import com.googlecode.fascinator.common.solr.SolrResult;
 import com.googlecode.fascinator.common.storage.StorageUtils;
+import com.googlecode.fascinator.portal.security.FascinatorWebSecurityExpressionRoot;
 import com.googlecode.fascinator.redbox.ws.HarvestFileMapService;
 import com.googlecode.fascinator.spring.ApplicationContextProvider;
-import com.hp.hpl.jena.query.function.library.not;
-import java.io.OutputStream;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiImplicitParam;
 import com.wordnik.swagger.annotations.ApiImplicitParams;
@@ -44,12 +44,13 @@ import com.wordnik.swagger.annotations.ApiResponses;
 public class HarvestResource extends RedboxServerResource {
 
 	private Storage storage;
-	private Map<String, DigitalObject> harvestFileMap = new HashMap<String, DigitalObject>();
 	private HarvestFileMapService harvestFileMapService;
+	private Logger log = LoggerFactory.getLogger(FascinatorWebSecurityExpressionRoot.class);
 
 	public HarvestResource() {
-		storage = (Storage) ApplicationContextProvider.getApplicationContext().getBean("fascinatorStorage");	
-		harvestFileMapService = (HarvestFileMapService) ApplicationContextProvider.getApplicationContext().getBean("harvestFileMapService");
+		storage = (Storage) ApplicationContextProvider.getApplicationContext().getBean("fascinatorStorage");
+		harvestFileMapService = (HarvestFileMapService) ApplicationContextProvider.getApplicationContext()
+				.getBean("harvestFileMapService");
 	}
 
 	@ApiOperation(value = "create a new ReDBox Object", tags = "object")
@@ -67,7 +68,15 @@ public class HarvestResource extends RedboxServerResource {
 		JSONArray recordsArray = batchJson.getArray("records");
 		for (Object recordObject : recordsArray) {
 			JsonObject processResult = processRecord((JsonObject) recordObject, packageType);
-			responses.add(processResult);
+			try {
+				responses.add(processResult);
+			} catch (Exception e) {
+				log.error("Error processing record in harvest", e);
+				JsonObject responseObject = new JsonObject();
+				responseObject.put("status", "error");
+				responseObject.put("reason", ExceptionUtils.getStackTrace(e));
+				responses.add(responseObject);
+			}
 		}
 		responseJsonObject.put("results", responses);
 		return new JsonSimple(responseJsonObject).toString();
@@ -75,6 +84,11 @@ public class HarvestResource extends RedboxServerResource {
 
 	private JsonObject processRecord(JsonObject record, String packageType)
 			throws IOException, PluginException, MessagingException {
+
+		JsonObject responseObject = validateRecord(record);
+		if (StringUtils.isNotBlank((String) responseObject.get("status"))) {
+			return responseObject;
+		}
 
 		String storageId = (String) record.get("oid");
 		String harvestId = (String) record.get("harvest_id");
@@ -88,26 +102,68 @@ public class HarvestResource extends RedboxServerResource {
 		}
 
 		JsonSimpleConfig config = new JsonSimpleConfig();
-		String harvestPath = "harvest/workflows/";
-		String repositoryName = "ReDBox";
 		String payloadId = "metadata.tfpackage";
-		String rulesConfig = config.getString(null, "portal", "packageTypes", packageType, "jsonconfig");
 
 		if ("mint".equals(config.getString(null, "system"))) {
-			harvestPath = "harvest/";
-			repositoryName = "Mint";
 			payloadId = "metadata.json";
-			rulesConfig = packageType + ".json";
 		} else {
 			if (!created) {
 				payloadId = findTFPackageId(storageId);
 			}
 		}
 
-		
-
 		DigitalObject recordObject = StorageUtils.getDigitalObject(storage, storageId);
+
+		setObjectMetadata(recordObject, record, packageType, created);
+
+		if (record.get("datastreams") != null) {
+			JSONArray datastreams = (JSONArray) record.get("datastreams");
+			for (Object object : datastreams) {
+				processDatastream(recordObject, (JsonObject) object);
+			}
+		}
+
+		JsonObject recordMetadata = (JsonObject) record.get("metadata");
+		if (StringUtils.isNotBlank(harvestId)) {
+			recordMetadata.put("harvestId", harvestId);
+		}
+		recordMetadata.put("packageType", packageType);
+
+		StorageUtils.createOrUpdatePayload(recordObject, payloadId,
+				IOUtils.toInputStream(recordMetadata.toString(), "utf-8"));
+
+		reindex(storageId);
+
+		responseObject.put("status", "success");
+		responseObject.put("oid", storageId);
+		if (StringUtils.isNotBlank(harvestId)) {
+			responseObject.put("harvest_id", harvestId);
+		}
+		responseObject.put("action", created ? "created" : "updated");
+		return responseObject;
+	}
+
+	private void processDatastream(DigitalObject recordObject, JsonObject datastream) throws StorageException {
+		String datastreamId = (String) datastream.get("datastream_id");
+		String contents = (String) datastream.get("contents");
+		StorageUtils.createOrUpdatePayload(recordObject, datastreamId, IOUtils.toInputStream(contents), "utf-8");
+	}
+
+	private Properties setObjectMetadata(DigitalObject recordObject, JsonObject record, String packageType,
+			boolean created) throws IOException, StorageException {
 		Properties objectMetadata = recordObject.getMetadata();
+		JsonSimpleConfig config = new JsonSimpleConfig();
+		String harvestPath = "harvest/workflows/";
+		String repositoryName = "ReDBox";
+
+		String rulesConfig = config.getString(null, "portal", "packageTypes", packageType, "jsonconfig");
+
+		if ("mint".equals(config.getString(null, "system"))) {
+			harvestPath = "harvest/";
+			repositoryName = "Mint";
+			rulesConfig = packageType + ".json";
+		}
+
 		if (created) {
 			File rulesConfigFile = FascinatorHome.getPathFile(harvestPath + rulesConfig);
 			JsonSimple rulesConfigJson = new JsonSimple(rulesConfigFile);
@@ -118,11 +174,11 @@ public class HarvestResource extends RedboxServerResource {
 
 			DigitalObject rulesConfigObject = harvestFileMapService.get(rulesConfigFile);
 			DigitalObject rulesObject = harvestFileMapService.get(rulesScriptFile);
-			objectMetadata.put("objectId", storageId);
+			objectMetadata.put("objectId", recordObject.getId());
 			objectMetadata.put("render-pending", "true");
 			objectMetadata.put("owner", "admin");
 			objectMetadata.put("repository.name", repositoryName);
-			
+
 			objectMetadata.put("repository.type", "Metadata Registry");
 			objectMetadata.put("metaPid", "TF-OBJ-META");
 			objectMetadata.setProperty("scriptType", scriptType);
@@ -131,30 +187,74 @@ public class HarvestResource extends RedboxServerResource {
 			objectMetadata.setProperty("rulesPid", rulesObject.getSourceId());
 			objectMetadata.setProperty("jsonConfigOid", rulesConfigObject.getId());
 			objectMetadata.setProperty("jsonConfigPid", rulesConfigObject.getSourceId());
-			
+
 			JsonObject params = rulesConfigJson.getObject("indexer", "params");
 			if (params != null) {
 				for (Object key : params.keySet()) {
 					objectMetadata.setProperty(key.toString(), params.get(key).toString());
 				}
 			}
-			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-			objectMetadata.store(outStream, "Written by Harvest API");
-			StorageUtils.createOrUpdatePayload(recordObject, "TF-OBJ-META",
-					IOUtils.toInputStream(new String(outStream.toByteArray()), "utf-8"));
+
 		}
-		JsonObject recordMetadata = (JsonObject)record.get("metadata");
-		if(StringUtils.isNotBlank(harvestId)) {
-			recordMetadata.put("harvestId", harvestId);
+
+		if (record.get("objectMetadata") != null) {
+			JsonObject data = (JsonObject) record.get("objectMetadata");
+			for (Object keyObject : data.keySet()) {
+				objectMetadata.put(keyObject, data.get(keyObject));
+			}
 		}
-		recordMetadata.put("packageType", packageType);
-		
-		StorageUtils.createOrUpdatePayload(recordObject, payloadId,
-				IOUtils.toInputStream(recordMetadata.toString(), "utf-8"));
-		
-		reindex(storageId);
+
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		objectMetadata.store(outStream, "Written by Harvest API");
+		StorageUtils.createOrUpdatePayload(recordObject, "TF-OBJ-META",
+				IOUtils.toInputStream(new String(outStream.toByteArray()), "utf-8"));
+
+		return objectMetadata;
+	}
+
+	private JsonObject validateRecord(JsonObject record) {
 		JsonObject responseObject = new JsonObject();
-		responseObject.put(storageId, "success");
+		if (StringUtils.isNotBlank((String) record.get("oid"))
+				&& StringUtils.isNotBlank((String) record.get("harvest_id"))) {
+			responseObject.put("status", "failed");
+			responseObject.put("reason", "Record is missing an oid or a harvest_id value");
+			return responseObject;
+		}
+
+		if (record.get("objectMetadata") != null) {
+			if (!(record.get("objectMetadata") instanceof JsonObject)) {
+				responseObject.put("status", "failed");
+				responseObject.put("reason", "objectMetadata property must be an object");
+				return responseObject;
+			}
+		}
+		
+		if (record.get("datastreams") != null) {
+			if (!(record.get("datastreams") instanceof JSONArray)) {
+				responseObject.put("status", "failed");
+				responseObject.put("reason", "datastreams property must be an array");
+				return responseObject;
+			} else {
+				for (Object datastreamObject : (JSONArray)record.get("datastreams")) {
+					if(!(datastreamObject instanceof JsonObject)) {
+						responseObject.put("status", "failed");
+						responseObject.put("reason", "All datastreams in the array must be objects");
+						return responseObject;
+					} else {
+						if(((JsonObject)datastreamObject).get("datastream_id") == null) {
+							responseObject.put("status", "failed");
+							responseObject.put("reason", "All datastreams in the array must have a datastream_id property");
+							return responseObject;
+						}
+						if(((JsonObject)datastreamObject).get("contents") == null) {
+							responseObject.put("status", "failed");
+							responseObject.put("reason", "All datastreams in the array must have a contents property");
+							return responseObject;
+						}
+					}
+				}
+			}
+		}
 		return responseObject;
 	}
 
@@ -173,7 +273,7 @@ public class HarvestResource extends RedboxServerResource {
 
 	private String findStorageId(String harvestId, String packageType) throws IndexerException, IOException {
 		Indexer indexer = (Indexer) ApplicationContextProvider.getApplicationContext().getBean("fascinatorIndexer");
-		SearchRequest request = new SearchRequest("harvestId:\"" + harvestId+"\"");
+		SearchRequest request = new SearchRequest("harvestId:\"" + harvestId + "\"");
 		request.addParam("fq", "packageType:" + packageType);
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 		indexer.search(request, byteArrayOutputStream);
@@ -190,21 +290,5 @@ public class HarvestResource extends RedboxServerResource {
 		return DigestUtils.md5Hex(
 				"SomeRandomPrefix:" + String.valueOf(System.currentTimeMillis()) + String.valueOf(Math.random()));
 	}
-
-//	private DigitalObject checkHarvestFile(Storage storage, File file) throws StorageException {
-//		String path = file.getPath();
-//
-//		DigitalObject fileObj = harvestFileMap.get(path);
-//
-//		if (fileObj == null) {
-//			fileObj = StorageUtils.checkHarvestFile(storage, file);
-//			if (fileObj == null) {
-//				String fileoid = StorageUtils.generateOid(file);
-//				fileObj = StorageUtils.getDigitalObject(storage, fileoid);
-//			}
-//			harvestFileMap.put(path, fileObj);
-//		}
-//		return fileObj;
-//	}
 
 }
